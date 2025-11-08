3dbda068356fbd580be68bc5d2265194d5848358
assis <69425038+grncd@users.noreply.github.com>
2025-11-08 15:32:06 -0300

    Change FFmpeg path in downloadsong.bat

--- downloadsong.bat
+++ downloadsong.bat
@@ -3,6 +3,7 @@ set "BAT_DIR=%~dp0"
 set "SPOTDL_EXE=%BAT_DIR%venv\Scripts\spotdl.exe"
 
 cd %2
-"%SPOTDL_EXE%" %1 --ffmpeg "C:\Users\%USERNAME%\AppData\Local\Programs\FFmpeg\bin\ffmpeg.exe"
+"%SPOTDL_EXE%" %1 --ffmpeg "C:\Program Files\FFmpeg\bin\ffmpeg.exe"
+
 
 
