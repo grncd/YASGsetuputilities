1072f33d1d60efcd42de8e19fdbd859a6e39d244
assis <arthurpiresassis@hotmail.com>
2025-11-09 20:25:41 -0300

    Update downloadsong.bat

--- downloadsong.bat
+++ downloadsong.bat
@@ -2,8 +2,11 @@
 set "BAT_DIR=%~dp0"
 set "SPOTDL_EXE=%BAT_DIR%venv\Scripts\spotdl.exe"
 
+if exist "C:\Program Files\FFmpeg\bin\ffmpeg.exe" (
+    set "FFMPEG_PATH=C:\Program Files\FFmpeg\bin\ffmpeg.exe"
+) else (
+    set "FFMPEG_PATH=C:\Users\%USERNAME%\AppData\Local\Programs\FFmpeg\bin\ffmpeg.exe"
+)
+
 cd %2
-"%SPOTDL_EXE%" %1 --ffmpeg "C:\Program Files\FFmpeg\bin\ffmpeg.exe"
-
-
-
+"%SPOTDL_EXE%" %1 --ffmpeg "%FFMPEG_PATH%"
