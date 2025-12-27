93faa8e74367a62937db97b134327cbab26b3156
assis <69425038+grncd@users.noreply.github.com>
2025-12-27 02:04:24 -0300

    linux fixes

--- getlyrics.sh
+++ getlyrics.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/venv/bin/activate"
-syrics "$@"
+
+# $1 is URL, $2 is DataPath
+if [ -n "$2" ]; then
+    cd "$2"
+fi
+
+syrics "$1"
