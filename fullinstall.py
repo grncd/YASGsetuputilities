eca2c6da9c4ab4c6447daf2bccbda5ad05046331
assis <69425038+grncd@users.noreply.github.com>
2026-01-16 02:07:02 -0300

    Remove installation step for 'syrics'
    Removed installation of the 'syrics' package.

--- fullinstall.py
+++ fullinstall.py
@@ -618,9 +618,6 @@ def main():
     install_git(progress_start=5)
     install_ffmpeg() # This now handles path correctly
 
-    print_progress(45, "Installing syrics")
-    run_command("python -m pip install syrics", "pip install syrics")
-
     print_progress(65, "Installing soundfile")
     run_command("python -m pip install soundfile", "pip install soundfile")
 
